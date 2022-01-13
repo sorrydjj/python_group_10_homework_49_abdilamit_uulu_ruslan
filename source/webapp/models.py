1fc1a603e7bd6ba5f7002db50967e8b553be247c
Ruslan <xenoncraftkg2@mail.ru>
2022-01-13 17:21:27 +0600

    сделана база данных

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    summary = models.CharField(max_length=200, null=False, blank=False, default="Unknown", verbose_name="Заголовок")
+    description = models.TextField(max_length=200, null=True, blank=True, verbose_name="Описанние")
+    stats = models.ForeignKey("webapp.Status",
+                               on_delete=models.PROTECT,
+                               related_name="status"
+                               )
+    types = models.ForeignKey("webapp.Type",
+                             on_delete=models.PROTECT,
+                             related_name="types"
+                             )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+
+    def __str__(self):
+        return f"{self.pk} | summary: {self.summary}"
+
+    class Meta:
+        db_table = "tasks"
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False, default="New")
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        db_table = "status"
+        verbose_name = "Статус"
+        verbose_name_plural = "Статусы"
+
+class Type(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False, default="Task")
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        db_table = "types"
+        verbose_name = "Тип"
+        verbose_name_plural = "Типы"
