e1c9e1ae421e5a0aaee1930c8d5431e00289f9fb
Ruslan <xenoncraftkg2@mail.ru>
2022-01-13 17:24:51 +0600

    добавлен просмотр главной страницы

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+from webapp.models import Task
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        task = Task.objects.order_by("created_at")
+        context = {"task": task}
+        return render(request, 'index.html', context)
