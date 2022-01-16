5286c8ad262db44963f24ceb389bda6bce6377fa
Ruslan <xenoncraftkg2@mail.ru>
2022-01-16 23:00:11 +0600

    добавлена радио кнопка в форме тип

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -7,6 +7,8 @@ from webapp.models import Type, Status
 class TaskForm(forms.Form):
     title = forms.CharField(max_length=200, required=True, label="Название")
     status = forms.ModelChoiceField(queryset=Status.objects.all(), empty_label="New", label="Статус")
-    type = forms.ModelChoiceField(queryset=Type.objects.all(), empty_label="Task", label="Тип")
-    description = forms.CharField(max_length=2000, required=True, label="Описание",
+    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(),  label="Тип",
+                                          widget=forms.CheckboxSelectMultiple,
+                                          required=True)
+    description = forms.CharField(max_length=2000, label="Описание",
                               widget=widgets.Textarea(attrs={"rows": 5, "cols":50}))
