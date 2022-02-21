92bb09becafff1a5af816103186a243d1caf41ca
Ruslan <xenoncraftkg2@mail.ru>
2022-02-21 16:11:50 +0600

    добавлен список пользователей

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import login_view, logout_view, RegisterView, UserProfileView
+from accounts.views import login_view, logout_view, RegisterView, UserProfileView, UsersView
 
 
 app_name = 'accounts'
@@ -11,4 +11,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name="logout"),
     path("register/", RegisterView.as_view(), name="registration"),
     path("<int:pk>/", UserProfileView.as_view(), name="user_profile"),
+    path("list", UsersView.as_view(), name="users_list")
 ]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,10 +1,12 @@
+
+
 from django.contrib.auth import authenticate, login, logout, get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, ListView
 
 from accounts.forms import MyUserCreationForm
 
@@ -72,3 +74,10 @@ class UserProfileView(LoginRequiredMixin, DetailView):
         kwargs['is_paginated'] = page.has_other_pages()
 
         return super(UserProfileView, self).get_context_data(**kwargs)
+
+
+class UsersView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    model = get_user_model()
+    template_name = "users_view.html"
+    context_object_name = "users"
+    permission_required = "auth.view_user"
