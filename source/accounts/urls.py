a2855329877888e7e7e108422b12ca09c2451fcf
Ruslan <xenoncraftkg2@mail.ru>
2022-02-24 16:30:00 +0600

    добавлено редактирование данных пользователья

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import login_view, logout_view, RegisterView, UserProfileView, UsersView
+from accounts.views import login_view, logout_view, RegisterView, UserProfileView, UsersView, UpdateUserView
 
 
 app_name = 'accounts'
@@ -11,5 +11,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name="logout"),
     path("register/", RegisterView.as_view(), name="registration"),
     path("<int:pk>/", UserProfileView.as_view(), name="user_profile"),
-    path("list", UsersView.as_view(), name="users_list")
+    path("list", UsersView.as_view(), name="users_list"),
+    path("update/", UpdateUserView.as_view(), name="user_update")
 ]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,16 +1,15 @@
-
-
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
-from accounts.forms import MyUserCreationForm
+from accounts.forms import MyUserCreationForm, ProfileUpdateForm, UserUpdateForm
 
 from accounts.models import Profile
+
+User = get_user_model()
 
 
 class RegisterView(CreateView):
@@ -81,3 +80,49 @@ class UsersView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     template_name = "users_view.html"
     context_object_name = "users"
     permission_required = "auth.view_user"
+
+
+class UpdateUserView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = UserUpdateForm
+    form_profile_class = ProfileUpdateForm
+    template_name = "update_profile.html"
+    context_object_name = "user_object"
+
+    def get_success_url(self):
+        return reverse("accounts:user_profile", kwargs={"pk": self.object.pk})
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        form = self.get_form()
+        profile_form = self.get_profile_form()
+
+        if form.is_valid() and profile_form.is_valid():
+            return self.form_valid(form, profile_form)
+        else:
+            return self.form_invalid(form, profile_form)
+
+    def form_valid(self, form, profile_form):
+        profile_form.save()
+        return super().form_valid(form)
+
+    def form_invalid(self, form, profile_form):
+        context = self.get_context_data(form=form, profile_form=profile_form)
+        return self.render_to_response(context)
+
+    def get_profile_form(self):
+        form_kwargs = {'instance': self.object.profile}
+        if self.request.method == 'POST':
+            form_kwargs['data'] = self.request.POST
+            form_kwargs['files'] = self.request.FILES
+        return ProfileUpdateForm(**form_kwargs)
+
+    def get_context_data(self, **kwargs):
+        if 'profile_form' not in kwargs:
+            kwargs['profile_form'] = self.get_profile_form()
+        return super().get_context_data(**kwargs)
+
