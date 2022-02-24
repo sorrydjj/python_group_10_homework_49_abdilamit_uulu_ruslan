e5720862ece366234057242d6ce85430fb0eaa41
Ruslan <xenoncraftkg2@mail.ru>
2022-02-24 17:28:34 +0600

    добавлено изменение паролья пользователя

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import login_view, logout_view, RegisterView, UserProfileView, UsersView, UpdateUserView
+from accounts.views import login_view, logout_view, RegisterView, UserProfileView, UsersView, UpdateUserView, UserPasswordChangeView
 
 
 app_name = 'accounts'
@@ -12,5 +12,7 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="registration"),
     path("<int:pk>/", UserProfileView.as_view(), name="user_profile"),
     path("list", UsersView.as_view(), name="users_list"),
-    path("update/", UpdateUserView.as_view(), name="user_update")
+    path("update/", UpdateUserView.as_view(), name="user_update"),
+    path("change-password/", UserPasswordChangeView.as_view(), name="change_password")
+
 ]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,13 +1,14 @@
-from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth import authenticate, login, logout, get_user_model, update_session_auth_hash
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
-from accounts.forms import MyUserCreationForm, ProfileUpdateForm, UserUpdateForm
+from accounts.forms import MyUserCreationForm, ProfileUpdateForm, UserUpdateForm, PasswordChangeForm
 
 from accounts.models import Profile
+
 
 User = get_user_model()
 
@@ -57,6 +58,7 @@ class UserProfileView(LoginRequiredMixin, DetailView):
     template_name = "profile.html"
     paginate_related_by = 5
     paginate_related_orphans = 0
+    context_object_name = "user_object"
 
     def get_context_data(self, **kwargs):
         paginator = Paginator(
@@ -125,4 +127,22 @@ class UpdateUserView(LoginRequiredMixin, UpdateView):
         if 'profile_form' not in kwargs:
             kwargs['profile_form'] = self.get_profile_form()
         return super().get_context_data(**kwargs)
+
+
+class UserPasswordChangeView(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    template_name = 'user_password_change.html'
+    form_class = PasswordChangeForm
+    context_object_name = 'user_object'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        update_session_auth_hash(self.request, self.object)
+        return response
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_success_url(self):
+        return reverse("accounts:user_profile", kwargs={"pk": self.request.user.pk})
 
